535faa6ca383afa4d734c0cfaeed4a6352a79e2c
sardylan <sardylan@gmail.com>
2020-02-11 19:57:04 +0100

    Fixes import of countries

--- ham/ham_utility/wizard/import_prefix.py
+++ ham/ham_utility/wizard/import_prefix.py
@@ -1,5 +1,6 @@
 import base64
 import io
+import logging
 import string
 
 import magic
@@ -8,26 +9,51 @@ from openpyxl import load_workbook
 from odoo import models, fields
 from odoo.exceptions import ValidationError
 
+LETTERS = string.digits + string.ascii_uppercase
+
+_logger = logging.getLogger(__name__)
+
 
 class ImportPrefix(models.TransientModel):
     _name = "ham_utility.wizard_import_prefix"
     _description = "Wizard user for import of official ITU XLSX countries file"
 
-    LETTERS = string.digits + string.ascii_uppercase
-
     xlsx_file = fields.Binary(
         string="XLSX File from ITU offical site"
     )
 
+    xlsx_file_name = fields.Char(
+        string="XLSX File name"
+    )
+
     def action_delete_all(self):
+        self.ensure_one()
+
+        _logger.info("Deleting all country prefixes")
         country_prefix_ids = self.env["ham_utility.country_prefix"].search([])
         country_prefix_ids.unlink()
 
+        _logger.info("Deleting all countries")
         country_ids = self.env["ham_utility.country"].search([])
         country_ids.unlink()
 
+    def action_open_web_site(self):
+        self.ensure_one()
+
+        url = "https://www.itu.int/en/ITU-R/terrestrial/fmd/Pages/call_sign_series.aspx"
+
+        return {
+            "type": "ir.actions.act_url",
+            "target": "new",
+            "url": url
+        }
+
     def action_import_xlsx(self):
         self.ensure_one()
+
+        country_obj = self.env["ham_utility.country"]
+        country_prefix_obj = self.env["ham_utility.country_prefix"]
+        res_country_obj = self.env["res.country"]
 
         rawfile = base64.b64decode(self.xlsx_file)
 
@@ -43,7 +69,7 @@ class ImportPrefix(models.TransientModel):
 
         fd = io.BytesIO(rawfile)
         wb = load_workbook(fd)
-        sheet = wb.get_active_sheet()
+        sheet = wb.active
         limits = sheet.dimensions.split(":")
 
         if limits[0] != "A1" or not limits[1].startswith("B"):
@@ -60,32 +86,51 @@ class ImportPrefix(models.TransientModel):
             raw_prefix = sheet["A%d" % r].value.strip()
             raw_country = sheet["B%d" % r].value.strip()
 
+            _logger.info("Parsing row %d: Prefix: %s - Country: %s" % (r, raw_prefix, raw_country))
+
             if raw_country not in country_prefixes:
                 country_prefixes[raw_country] = []
 
             country_prefixes[raw_country].append(raw_prefix)
 
         for country in country_prefixes:
-            country_id = self.env["ham_utility.country"].search([
+            country_id = country_obj.search([
                 ("name", "=", country)
             ], limit=1)
 
-            if len(country_id) == 0:
-                country_id = self.env["ham_utility.country"].create({
+            if not country_id:
+                _logger.info("Creating country %s" % country)
+
+                country_id = country_obj.create({
                     "name": country
                 })
+
+            country_name = country_id.name.split(" ")[0].strip()
+
+            res_country_id = res_country_obj.search([("name", "ilike", country_name)])
+            if len(res_country_id) == 1:
+                _logger.info("Association with res.country %d: %s" % (res_country_id.id, res_country_id.name))
+                country_id.res_country_id = res_country_id.id
 
             prefixes = self._reduce_prefixes(country_prefixes[country])
 
             for prefix in prefixes:
-                self.env["ham_utility.country_prefix"].create({
-                    "prefix": prefix,
-                    "country_id": country_id.id,
-                })
+                country_prefix_id = country_prefix_obj.search([
+                    ("country_id", "=", country_id.id),
+                    ("prefix", "=", prefix)
+                ])
+
+                if not country_prefix_id:
+                    _logger.info("Adding country prefix %s to %s" % (prefix, country_id.name))
+
+                    country_prefix_obj.create({
+                        "prefix": prefix,
+                        "country_id": country_id.id,
+                    })
 
         return {
             "type": "ir.actions.client",
-            "tag": "reload",
+            "tag": "reload"
         }
 
     @staticmethod
