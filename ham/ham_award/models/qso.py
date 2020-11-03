f361f45df62d7a8f71a9009bc25a7e05ca977874
sardylan <sardylan@gmail.com>
2020-11-03 18:37:36 +0100

    Adds Award QSO publish model

--- ham/ham_award/models/qso.py
+++ ham/ham_award/models/qso.py
@@ -1,6 +1,4 @@
 from odoo import models, fields
-from odoo.exceptions import ValidationError
-from odoo.tools.translate import _
 
 
 class QSO(models.Model):
@@ -42,58 +40,34 @@ class QSO(models.Model):
         tracking=True
     )
 
-    hrdlog_sent = fields.Boolean(
-        string="Sent to HRDLog",
-        help="Sent to HRDLog",
-        required=True,
-        default=False,
-        readonly=True,
-        tracking=True
-
+    publish_ids = fields.One2many(
+        string="Pubblications",
+        help="QSO pubblications in online websites",
+        comodel_name="ham.award.qso.publish",
+        inverse_name="qso_id",
+        readonly=True
     )
 
-    hrdlog_ts_sent = fields.Datetime(
-        string="Date and Time HRDLog",
-        help="Date and Time of upload to HRDLog",
-        readonly=True,
-        tracking=True
+
+class QSOPublish(models.Model):
+    _name = "ham.award.qso.publish"
+    _description = "QSO Sending to external sites"
+
+    qso_id = fields.Many2one(
+        string="QSO",
+        help="QSO",
+        comodel_name="ham.award.qso",
+        required=True
     )
 
-    def action_set_hrdlog_sent(self):
-        for rec in self:
-            self.set_hrdlog_sent(rec)
+    website = fields.Char(
+        string="Website",
+        help="External website",
+        required=True
+    )
 
-    def set_hrdlog_sent(self, qso):
-        if not qso:
-            raise ValidationError(_("Invalid QSO"))
-
-        qso.write({
-            "hrdlog_ts_sent": fields.Datetime.now(),
-            "hrdlog_sent": True
-        })
-
-    # station_ids = fields.Many2many(
-    #     string="Referenced Stations",
-    #     help="Referenced Stations",
-    #     comodel_name="ham_award.station",
-    #     relation="ham_award_qso_station_rel",
-    #     column1="qso_id",
-    #     column2="station_id",
-    #     compute="_compute_station_ids"
-    # )
-
-    # @api.depends("callsign")
-    # def _compute_station_ids(self):
-    #     station_obj = self.env["ham_award.station"]
-    #
-    #     for rec in self:
-    #         if not rec.callsign:
-    #             continue
-    #
-    #         callsign = sorted(re.split(r"([A-Z0-9]+)", rec.callsign.strip().upper()), key=len, reverse=True)[0]
-    #
-    #         station_ids = station_obj.search([
-    #             ("callsign", "ilike", callsign)
-    #         ])
-    #
-    #         rec.station_ids = [(6, 0, station_ids.ids)]
+    ts = fields.Datetime(
+        string="Date & Time",
+        help="QSO publishing Date & Time",
+        readonly=True
+    )
