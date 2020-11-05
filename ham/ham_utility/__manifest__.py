0b49a4039ce32e58acc40931de822984ea9c7bf8
Luca Cireddu <luca.cireddu@flosslab.com>
2020-11-05 19:58:24 +0100

    Version increment

--- ham/ham_utility/__manifest__.py
+++ ham/ham_utility/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "HAM Utility",
-    "version": "14.0.2.0.0",
+    "version": "14.0.2.1.0",
     "category": "HAM Utility",
     "summary": "HAM utilities and models",
     "sequence": 0,
